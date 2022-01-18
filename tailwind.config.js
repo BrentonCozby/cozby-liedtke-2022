0f2ea1c4b93d69f6ba3a6a8c807e878188c5e5fa
Brenton Cozby <5513633+BrentonCozby@users.noreply.github.com>
2022-01-17 23:41:08 -0500

    add color palette to tw theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lc-gold': '#D4B759',
+        'lc-pink': '#F8D3C5',
+        'lc-sage': '#A3B899',
+        'lc-forest': '#345859',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
